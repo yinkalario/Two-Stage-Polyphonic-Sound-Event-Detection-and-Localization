ec1c2de7a3d19efea09e46789502395a44655bae
Jee Wen Jie <40534826+jeewenjie@users.noreply.github.com>
2019-06-13 14:58:06 +0800

    Update 3_evaluation.sh
    Set directories manually in each separate bash script as 0_directories.sh might not work for everyone.
    added extra argument, "--dataset_dir=$DATASET_DIR"

--- 3_evaluation.sh
+++ 3_evaluation.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# Data directory
+DATASET_DIR='/vol/vssp/AP_datasets/audio/dcase2019/task3/dataset_root/'
+
+# Feature directory
+FEATURE_DIR='/vol/vssp/msos/YinC/workspace/Dataset_Features/DCASE2019/task3/'
+
+# Workspace
+WORKSPACE='/vol/vssp/msos/YinC/workspace/DCASE2019/task3/'
+cd $WORKSPACE
+
 FEATURE_TYPE='logmelgcc'
 AUDIO_TYPE='mic'
 SEED=10
@@ -19,8 +29,8 @@ GPU_ID=0
 for FOLD in {1..4}
     do
     echo $'\nFold: '$FOLD
-    CUDA_VISIBLE_DEVICES=$GPU_ID python ${WORKSPACE}main.py inference --workspace=$WORKSPACE --feature_dir=$FEATURE_DIR --feature_type=$FEATURE_TYPE --audio_type=$AUDIO_TYPE --task_type=$TASK_TYPE --fold=$FOLD --iteration=$ITERATION --seed=$SEED
+    CUDA_VISIBLE_DEVICES=$GPU_ID python ${WORKSPACE}main.py inference --workspace=$WORKSPACE --feature_dir=$FEATURE_DIR --dataset_dir=$DATASET_DIR --feature_type=$FEATURE_TYPE --audio_type=$AUDIO_TYPE --task_type=$TASK_TYPE --fold=$FOLD --iteration=$ITERATION --seed=$SEED
 done
 
 # inference all folds
-python ${WORKSPACE}main.py inference_all --workspace=$WORKSPACE --audio_type=$AUDIO_TYPE --task_type=$TASK_TYPE --seed=$SEED
+python ${WORKSPACE}main.py inference_all --workspace=$WORKSPACE --dataset_dir=$DATASET_DIR --audio_type=$AUDIO_TYPE --task_type=$TASK_TYPE --seed=$SEED
