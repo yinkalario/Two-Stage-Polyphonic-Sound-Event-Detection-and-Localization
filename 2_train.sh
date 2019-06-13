2cccd166a96583373cbb93755153a9a0e18e4fad
Jee Wen Jie <40534826+jeewenjie@users.noreply.github.com>
2019-06-13 14:54:08 +0800

    Update 2_train.sh
    bash 0_directories.sh might not work for everyone.

--- 2_train.sh
+++ 2_train.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# Data directory
+DATASET_DIR='/vol/vssp/AP_datasets/audio/dcase2019/task3/dataset_root/'
+
+# Feature directory
+FEATURE_DIR='/vol/vssp/msos/YinC/workspace/Dataset_Features/DCASE2019/task3/'
+
+# Workspace
+WORKSPACE='/vol/vssp/msos/YinC/workspace/DCASE2019/task3/'
+cd $WORKSPACE
+
 FEATURE_TYPE='logmelgcc'
 AUDIO_TYPE='mic'
 SEED=10
